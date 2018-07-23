bae3b55827d3c59c440374b2ea2f4ab1f409b28c
sheik <sheik.abdullah@inapp.com>
2018-07-23 15:46:28 +0530

    setup base

---
+++ application/modules/admin/controllers/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+(defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/**
+ * Admin Backend
+ *
+ * @author Sheik Abdullah
+ */
+class Admin extends MY_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        $this->load->view('login');
+    }
+
+}
---
+++ application/modules/api/controllers/Api.php
@@ -0,0 +1,26 @@
+<?php
+
+(defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/**
+ * Description of site
+ *
+ * @author https://www.roytuts.com
+ */
+class Api extends MY_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function index() {
+        //$this->load->view('home');
+    }
+
+     function user() {
+     	echo 'user response';
+        //$this->load->view('home');
+    }
+
+
+}
