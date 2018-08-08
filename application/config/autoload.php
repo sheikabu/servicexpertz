8536f21b143020b90fc5ed821cb6540eca6b30b0
franklininnocent <franklininnocent.fs@gmail.com>
2018-08-08 11:51:49 +0530

    refresh token

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -92,7 +92,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url', 'authentication_helper');
 //$autoload['helper'] = array('authentication_helper'); //Franklin
 
 
