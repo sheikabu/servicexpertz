919d9e3e54037b40600daf22490f76848b340970
franklininnocent <franklininnocent.fs@gmail.com>
2018-08-27 22:26:10 +0530

    contact us api

--- application/modules/api/controllers/Category.php
+++ application/modules/api/controllers/Category.php
@@ -8,13 +8,14 @@ class Category extends MY_Controller {
     function __construct() {
         parent::__construct();
 		header('Access-Control-Allow-Origin: *');
-		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
         header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
         header('Access-Control-Max-Age: 1000');
 		$method = $_SERVER['REQUEST_METHOD'];
         if($method == "OPTIONS") {
             die();
         }
+        
 		$this->load->model('api/common_model');
     }
 
